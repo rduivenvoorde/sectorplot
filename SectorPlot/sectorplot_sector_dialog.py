f618485bb01ae7726d3b08938737011659dbcbfc
Richard Duivenvoorde <richard@duif.net>
2015-11-16 07:14:31 +0100

    forgotten dialog

---
+++ SectorPlot/sectorplot_sector_dialog.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ SectorPlotSectorDialog
+                                 A QGIS plugin
+ Plots sector areas s on risk management map
+                             -------------------
+        begin                : 2015-11-10
+        git sha              : $Format:%H$
+        copyright            : (C) 2015 by RIVM
+        email                : marnix.de.ridder@rivm.nl
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+import os
+
+from PyQt4 import QtGui, uic
+
+# this magic let compile Qt the ui files on the fly (if path known)
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'sectorplot_sector_dialog_base.ui'))
+
+# this determines the Class name base on the FORM_CLASS above
+class SectorPlotSectorDialog(QtGui.QDialog, FORM_CLASS):
+    def __init__(self, parent=None):
+        """Constructor."""
+        super(SectorPlotSectorDialog, self).__init__(parent)
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.setupUi(self)
