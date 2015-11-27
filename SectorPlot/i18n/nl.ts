a0c5dce221b79499e386a10949fafed242390576
Richard Duivenvoorde <richard@duif.net>
2015-11-27 13:56:54 +0100

    adding dutch as translation

---
+++ SectorPlot/i18n/nl.ts
@@ -0,0 +1,390 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="test_translations.py" line="48"/>
+        <source>Good morning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="countermeasures.py" line="41"/>
+        <source>Evacuatie</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="countermeasures.py" line="42"/>
+        <source>Onmiddelijke evacuatie</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="countermeasures.py" line="44"/>
+        <source>Schuilen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="countermeasures.py" line="45"/>
+        <source>Jodiumprofylaxe tot 18 jaar en zwangeren</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="countermeasures.py" line="46"/>
+        <source>Jodiumprofylaxe tot 40 jaar en zwangeren</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="countermeasures.py" line="47"/>
+        <source>Relocatie</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="countermeasures.py" line="49"/>
+        <source>Landbouwmaatregelen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="countermeasures.py" line="50"/>
+        <source>Graasverbod</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="countermeasures.py" line="51"/>
+        <source>Sluiten van kassen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="countermeasures.py" line="53"/>
+        <source>Voedselbeperking</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="countermeasures.py" line="54"/>
+        <source>Drinkwaterbeperking</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="countermeasures.py" line="55"/>
+        <source>Beregening verbod onbegroeid land/weiland</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="countermeasures.py" line="56"/>
+        <source>Beregening verbod begroeid land/weiland</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="countermeasures.py" line="57"/>
+        <source>Zuiveringsslib verbod</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="countermeasures.py" line="59"/>
+        <source>Overige maatregelen</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="sectorplot_sector_dialog_base.ui" line="14"/>
+        <source>Sectorplot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sector_dialog_base.ui" line="20"/>
+        <source>Custom Min Distance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sector_dialog_base.ui" line="27"/>
+        <source>Angle (degrees)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sector_dialog_base.ui" line="34"/>
+        <source>Counter Meausure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sector_dialog_base.ui" line="41"/>
+        <source>Direction (start, degrees)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sector_dialog_base.ui" line="98"/>
+        <source>00000; </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sector_dialog_base.ui" line="121"/>
+        <source>0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sector_dialog_base.ui" line="61"/>
+        <source>Distance (max, km)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sector_dialog_base.ui" line="71"/>
+        <source>Distance (min, km)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sector_dialog_base.ui" line="78"/>
+        <source>Sector name (optional)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sector_dialog_base.ui" line="118"/>
+        <source>000; </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sectorplotset_dialog_base.ui" line="14"/>
+        <source>New Sectorplot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sectorplotset_dialog_base.ui" line="20"/>
+        <source>[sector plot name + location]</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sectorplotset_dialog_base.ui" line="27"/>
+        <source>Sectorplot Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sectorplotset_dialog_base.ui" line="34"/>
+        <source>Id:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sectorplotset_dialog_base.ui" line="41"/>
+        <source>Sectors:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sectorplotset_dialog_base.ui" line="48"/>
+        <source>[name]</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sectorplotset_dialog_base.ui" line="55"/>
+        <source>[on save]</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sectorplotset_dialog_base.ui" line="62"/>
+        <source>Location</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sectorplotset_dialog_base.ui" line="69"/>
+        <source>hh:mm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sectorplotset_dialog_base.ui" line="79"/>
+        <source>Save Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sectorplotset_dialog_base.ui" line="86"/>
+        <source>Counter Measure Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sectorplotset_dialog_base.ui" line="96"/>
+        <source>Open Selected Sector</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sectorplotset_dialog_base.ui" line="103"/>
+        <source>[id]</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sectorplotset_dialog_base.ui" line="117"/>
+        <source>dd-MM-yyyy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sectorplotset_dialog_base.ui" line="133"/>
+        <source>Remove Selected Sector</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sectorplotset_dialog_base.ui" line="150"/>
+        <source>Add New Sector</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SectorPlot</name>
+    <message>
+        <location filename="sectorplot.py" line="73"/>
+        <source>SectorPlot Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="248"/>
+        <source>&amp;SectorPlot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="239"/>
+        <source>Sector plot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="259"/>
+        <source>This Plugin ONLY works when you have OTF (On The Fly Reprojection) enabled for current QGIS Project.
+
+Please enable OTF for this project or open a project with OTF enabled.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="267"/>
+        <source>Sector Layer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="312"/>
+        <source>Id</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="313"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="314"/>
+        <source>Save Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="315"/>
+        <source>Countermeasure Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="357"/>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="378"/>
+        <source>Countrycode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="379"/>
+        <source>Site</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="380"/>
+        <source>Block</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="405"/>
+        <source>Lon: %s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="405"/>
+        <source> Lat: %s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="538"/>
+        <source>Sector name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="539"/>
+        <source>Countermeasure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="540"/>
+        <source>MinDist (km)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="541"/>
+        <source>Distance (km)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="542"/>
+        <source>Direction (deg)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="543"/>
+        <source>Angle (deg)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SectorPlotNPPDialog</name>
+    <message>
+        <location filename="sectorplot_location_dialog_base.ui" line="17"/>
+        <source>Set Sectorplot Location</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_location_dialog_base.ui" line="23"/>
+        <source>Latitude</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_location_dialog_base.ui" line="37"/>
+        <source>Longitude</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_location_dialog_base.ui" line="71"/>
+        <source>Nuclear Power Plant Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_location_dialog_base.ui" line="91"/>
+        <source>Or</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_location_dialog_base.ui" line="98"/>
+        <source>Location From Map</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_location_dialog_base.ui" line="112"/>
+        <source>Click in map to set the sectorplot location</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SectorPlotSetsDialog</name>
+    <message>
+        <location filename="sectorplot_sectorplotsets_dialog_base.ui" line="50"/>
+        <source>Recent Sectorplots</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sectorplotsets_dialog_base.ui" line="23"/>
+        <source>Copy Selected Sectorplot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sectorplot_sectorplotsets_dialog_base.ui" line="43"/>
+        <source>New Sectorplot</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
