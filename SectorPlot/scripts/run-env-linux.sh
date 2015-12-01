42d010073561873194990ee556f53abfb6962299
Richard Duivenvoorde <richard@duif.net>
2015-12-01 09:56:50 +0100

    env script

--- SectorPlot/scripts/run-env-linux.sh
+++ SectorPlot/scripts/run-env-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-QGIS_PREFIX_PATH=/usr/local/qgis-2.0
+QGIS_PREFIX_PATH=/home/richard/apps/qgis/master/debug
 if [ -n "$1" ]; then
     QGIS_PREFIX_PATH=$1
 fi
@@ -15,12 +15,12 @@ export PYTHONPATH=${QGIS_PREFIX_PATH}/share/qgis/python:${QGIS_PREFIX_PATH}/shar
 
 echo "QGIS PATH: $QGIS_PREFIX_PATH"
 export QGIS_DEBUG=0
-export QGIS_LOG_FILE=/tmp/inasafe/realtime/logs/qgis.log
+export QGIS_LOG_FILE=/tmp/qgis.log
 
 export PATH=${QGIS_PREFIX_PATH}/bin:$PATH
 
 echo "This script is intended to be sourced to set up your shell to"
-echo "use a QGIS 2.0 built in $QGIS_PREFIX_PATH"
+echo "use a QGIS built in $QGIS_PREFIX_PATH"
 echo
 echo "To use it do:"
 echo "source $BASH_SOURCE /your/optional/install/path"
