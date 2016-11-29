54e99308dbdea99cba0e6ba6c70ab690e5f2411a
Richard Duivenvoorde <richard@duif.net>
2016-11-29 13:22:51 +0100

    v0.5

--- SectorPlot/i18n/nl.ts
+++ SectorPlot/i18n/nl.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="nl">
+<TS version="2.1" language="nl">
 <context>
     <name>@default</name>
     <message>
@@ -211,26 +211,108 @@
         <source>Countermeasure Time</source>
         <translation>Tegenmaatregeltijd</translation>
     </message>
+    <message>
+        <location filename="sectorplot_settings_dialog_base.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialoog</translation>
+    </message>
+    <message>
+        <location filename="sectorplot_settings_dialog_base.ui" line="24"/>
+        <source>Postgis Database Details</source>
+        <translation>Postgis Database Details</translation>
+    </message>
+    <message>
+        <location filename="sectorplot_settings_dialog_base.ui" line="30"/>
+        <source>Postgis Database connection details</source>
+        <translation>Postgis Database verbindingsgegevens</translation>
+    </message>
+    <message>
+        <location filename="sectorplot_settings_dialog_base.ui" line="39"/>
+        <source>Database</source>
+        <translation>Database</translation>
+    </message>
+    <message>
+        <location filename="sectorplot_settings_dialog_base.ui" line="113"/>
+        <source>Host</source>
+        <translation>Host</translation>
+    </message>
+    <message>
+        <location filename="sectorplot_settings_dialog_base.ui" line="106"/>
+        <source>User</source>
+        <translation>Gebruiker</translation>
+    </message>
+    <message>
+        <location filename="sectorplot_settings_dialog_base.ui" line="126"/>
+        <source>Password</source>
+        <translation>Wachtwoord</translation>
+    </message>
+    <message>
+        <location filename="sectorplot_settings_dialog_base.ui" line="136"/>
+        <source>Test</source>
+        <translation>Test</translation>
+    </message>
+    <message>
+        <location filename="sectorplot_settings_dialog_base.ui" line="94"/>
+        <source>Geoserver Details</source>
+        <translation>Geoserver Details</translation>
+    </message>
+    <message>
+        <location filename="sectorplot_settings_dialog_base.ui" line="100"/>
+        <source>Geoserver REST connection details</source>
+        <translation>Geoserver REST verbindingsgegevens</translation>
+    </message>
+    <message>
+        <location filename="sectorplot_settings_dialog_base.ui" line="154"/>
+        <source>Information</source>
+        <translation>Informatie</translation>
+    </message>
+    <message>
+        <location filename="sectorplot_settings_dialog_base.ui" line="160"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Cantarell&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;The Sectorplot plugin&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;CalNet wiki page:&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;- https://wiki.cal-net.nl/display/Handleiding/Handleidingen&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;To see SectorPlots in JRodos, use a url like below:&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;- http://geoserver.dev.cal-net.nl/sectorplots&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Cantarell&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;The Sectorplot plugin&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;CalNet wiki pagina:&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;- https://wiki.cal-net.nl/display/Handleiding/Handleidingen&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Om SectorPlots in JRodos te kunnen zien, gebruikt u een url zoals:&lt;/p&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;- http://geoserver.dev.cal-net.nl/sectorplots&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
 </context>
 <context>
     <name>SectorPlot</name>
     <message>
-        <location filename="sectorplot.py" line="75"/>
+        <location filename="sectorplot.py" line="78"/>
         <source>SectorPlot Plugin</source>
         <translation>SectorPlot Plugin</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="274"/>
+        <location filename="sectorplot.py" line="298"/>
         <source>&amp;SectorPlot</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="265"/>
+        <location filename="sectorplot.py" line="277"/>
         <source>Sector plot</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="291"/>
+        <location filename="sectorplot.py" line="315"/>
         <source>This Plugin ONLY works when you have OTF (On The Fly Reprojection) enabled for current QGIS Project.
 
 Please enable OTF for this project or open a project with OTF enabled.</source>
@@ -240,136 +322,136 @@ Please enable OTF for this project or open a project with OTF enabled.</source>
 Zet OTF voor dit project alstublieft aan.</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="399"/>
+        <location filename="sectorplot.py" line="425"/>
         <source>Id</source>
         <translation>Id</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="400"/>
+        <location filename="sectorplot.py" line="426"/>
         <source>Name</source>
         <translation>Naam</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="402"/>
+        <location filename="sectorplot.py" line="428"/>
         <source>Save Time</source>
         <translation>Tijd van Opslaan</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="401"/>
+        <location filename="sectorplot.py" line="427"/>
         <source>Countermeasure Time</source>
         <translation>Tegenmaatregeltijd</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="527"/>
+        <location filename="sectorplot.py" line="553"/>
         <source>Search</source>
         <translation>Zoeken</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="548"/>
+        <location filename="sectorplot.py" line="574"/>
         <source>Countrycode</source>
         <translation>Landcode</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="549"/>
+        <location filename="sectorplot.py" line="575"/>
         <source>Site</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="550"/>
+        <location filename="sectorplot.py" line="576"/>
         <source>Block</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="449"/>
+        <location filename="sectorplot.py" line="475"/>
         <source>Lon: %s</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="449"/>
+        <location filename="sectorplot.py" line="475"/>
         <source> Lat: %s</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="678"/>
+        <location filename="sectorplot.py" line="704"/>
         <source>Sector name</source>
         <translation>Sectornaam</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="679"/>
+        <location filename="sectorplot.py" line="705"/>
         <source>Countermeasure</source>
         <translation>Tegenmaatregel</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="680"/>
+        <location filename="sectorplot.py" line="706"/>
         <source>MinDist (km)</source>
         <translation>MinDist (km)</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="681"/>
+        <location filename="sectorplot.py" line="707"/>
         <source>Distance (km)</source>
         <translation>Afstand (km)</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="682"/>
+        <location filename="sectorplot.py" line="708"/>
         <source>Direction (deg)</source>
         <translation>Richting (graden)</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="683"/>
+        <location filename="sectorplot.py" line="709"/>
         <source>Angle (deg)</source>
         <translation>Hoek (graden)</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="615"/>
+        <location filename="sectorplot.py" line="641"/>
         <source>One of the coordinates is not valid.
 Please check and correct.</source>
         <translation>Een van de coördinaten is niet geldig.
 Check en verbeter die alstublieft.</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="746"/>
+        <location filename="sectorplot.py" line="772"/>
         <source>You did not provide a name for this Sectorplot. 
 Please provide one.</source>
         <translation>U heeft geen naam gegeven voor deze Sectorplot.
 Geef een naam alstublieft.</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="752"/>
+        <location filename="sectorplot.py" line="778"/>
         <source>You did not provide a sector for this Sectorplot. 
 Please provide at least one.</source>
         <translation>Deze Sectorplot heeft niet één sector.
 Maak minstens één sector aan alstublieft.</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="817"/>
+        <location filename="sectorplot.py" line="846"/>
         <source>The Direction value is not valid [%s, %s].
 Please check and correct.</source>
         <translation>De waarde van Richting is niet geldig [%s, %s].
 Check en pas aan alstublieft.</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="822"/>
+        <location filename="sectorplot.py" line="851"/>
         <source>The Angle value is not valid [%s, %s].
 Please check and correct.</source>
         <translation>De waarde van Hoek is niet geldig [%s, %s].
 Check en pas aan alstublieft.</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="827"/>
+        <location filename="sectorplot.py" line="856"/>
         <source>The Distance value is not valid [%s, %s].
 Please check and correct.</source>
         <translation>De waarde van Afstand is niet geldig [%s, %s].
 Check en pas aan alstublieft.</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="832"/>
+        <location filename="sectorplot.py" line="861"/>
         <source>The Min Distance value is not valid [%s, %s].
 Please check and correct.</source>
         <translation>De waarde van Min Afstand is niet geldig [%s, %s].
 Check en pas aan alstublieft.</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="837"/>
+        <location filename="sectorplot.py" line="866"/>
         <source>The Min Distance value is not valid:
 Min Distance value is bigger then Distance value.
 Please check and correct.</source>
@@ -378,57 +460,79 @@ De Min Afstand is groter dan de Afstand waarde.
 Check en pas aan alstublieft.</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="79"/>
+        <location filename="sectorplot.py" line="82"/>
         <source>Sector Plugin Layer</source>
         <translation>SectorPluginlaag</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="376"/>
+        <location filename="sectorplot.py" line="402"/>
         <source>Database error:
 %s</source>
         <translation>Databasefout:
 %s</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="465"/>
+        <location filename="sectorplot.py" line="491"/>
         <source>Date Time conversion problem</source>
         <translation>Datum-Tijd conversieprobleem</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="489"/>
+        <location filename="sectorplot.py" line="515"/>
         <source>Save shapefile as</source>
         <translation>Opslaan als shapefile</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="506"/>
+        <location filename="sectorplot.py" line="532"/>
         <source>Problem saving shapefile</source>
         <translation>Fout bij het opslaan als shapefile</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="771"/>
+        <location filename="sectorplot.py" line="800"/>
         <source>Database error:
  %s</source>
         <translation>Databasefout:
 %s</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="481"/>
+        <location filename="sectorplot.py" line="507"/>
         <source>WMS layer created successfully.</source>
         <translation>WMS-laag aanmaken geslaagd.</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="483"/>
+        <location filename="sectorplot.py" line="509"/>
         <source>Problem creating WMS layer
 (Maybe already published? A SectorPlot cannot be republished...)</source>
         <translation>Fout bij het aanmaken van de WMS-laag.
 (Misschien al gepubliceerd? Een SectorPlot kan maar 1 keer worden gepubliceerd...)</translation>
     </message>
     <message>
-        <location filename="sectorplot.py" line="503"/>
+        <location filename="sectorplot.py" line="529"/>
         <source>Shapefile created successfully:
  </source>
         <translation>Shapefile aanmaken gelukt:
 </translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="285"/>
+        <source>Show Settings</source>
+        <translation>Toon Instellingen</translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="796"/>
+        <source>Demo mode
+Sectorplot is not saved to WMS or Database</source>
+        <translation>Demo mode
+Sectorplot wordt niet opgeslagen in WMS of Database</translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="927"/>
+        <source>Connection successful</source>
+        <translation>Verbinding succes</translation>
+    </message>
+    <message>
+        <location filename="sectorplot.py" line="929"/>
+        <source>Connection problem, please check inputs</source>
+        <translation>Probleem met verbinden, controleer de invoer</translation>
     </message>
 </context>
 <context>
